5959669581464257aa0ede5201e20a71f5d6790c
zlapot <zloi.apot@yandex.ru>
2016-10-05 14:35:23 +0300

    add block "Share with friend"

--- views/site/index.php
+++ views/site/index.php
@@ -336,6 +336,9 @@ $this->title = 'My Yii Application';
             <div class="modal-autor"></div>
           </div>
           <div class="modal-footer">
+          <script src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script>
+            <script src="//yastatic.net/share2/share.js"></script>
+            <div class="ya-share2" data-services="vkontakte,facebook,odnoklassniki,twitter"></div>
             <button type="button" class="btn btn-primary" data-dismiss="modal">Закрыть</button>
           </div>
         </div>
