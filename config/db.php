eb1e347bed6e2479d3c3a27a0539afb661c2da83
ALEX <zloi.apot@yandex.ua>
2016-10-12 01:43:36 +0300

    change chmod

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=poem',
     'username' => 'root',
-    'password' => '',
+    'password' => 'meizu',
     'charset' => 'utf8',
 ];
 
