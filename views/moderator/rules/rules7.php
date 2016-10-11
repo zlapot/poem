a622f79d80deb221064d748e9f75dde97ee82970
ALEX <zloi.apot@yandex.ua>
2016-10-12 01:26:09 +0300

    complete all local

---
+++ views/moderator/rules/rules7.php
@@ -0,0 +1,20 @@
+<div class="col-md-4 offset-right blockquote-reverse">
+	Wir bringen Ihnen die Regeln unsrer Seite (für deutschsprachige Genossen)
+	Wir bringen auch unsere Entschuldigungen für unsre Fehler in Deutsch
+	<ul class="poem-rule">
+		<li class="item-rule">Keine Fluchen</li>
+		<li class="item-rule">Beschimpfen Sie nicht andere Teilnehmern!</li>
+		<li class="item-rule">Eure subjektive Meinung soll die andere nicht stören</li>
+		<li class="item-rule">Halten Sie Ihren Versfuß beim Schreiben!</li>
+		<li class="item-rule">Keine Fehler im Schreiben,wenn Sie in eurer Muttersprache
+		die Gedichte schaffen!</li>
+		<li class="item-rule">Achten Sie Abdul,euren heiligen Gott!!</li>
+		<li class="item-rule">Natürlich,keine Politik</li>
+		<li class="item-rule">Unser Admin ist Süüüüüüßi und Sie sollen ihn lieb haben. 
+		Das ist zwar gut,beruhigen Sie nicht</li>
+		<li class="item-rule">Die weltvolle werden ermuntert</li>
+		<li class="item-rule">Die Physikgesetze sind Gesetze der Welt.Sie dürfen sie nicht verletzen</li>
+		<li class="item-rule">Herzlich willkommen! Stellen Sie uns eure Fragen.Wir sind sehr froh,
+		auf sie zu antworten</li>
+	</ul>
+</div>
