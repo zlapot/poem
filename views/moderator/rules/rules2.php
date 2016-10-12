2a196cbf7843bdfad6dc6032f68187143c75a63a
zlapot <zloi.apot@yandex.ru>
2016-10-12 18:10:23 +0300

    some bugfix

--- views/moderator/rules/rules2.php
+++ views/moderator/rules/rules2.php
@@ -1,19 +1,18 @@
 <div class="col-md-4 offset-right blockquote-reverse">
-	Regulile acestui site pentru tovarăși romănești. Vă rog,mă scuzați,dacă veți
-	vedea greșeli
+	Regulile acestui site pentru tovarăși români:
 	<ul class="poem-rule">
-		<li class="item-rule">Nu utilisați obsceni cuvânte</li>
+		<li class="item-rule">Nu utilisați cuvinte obscene</li>
 		<li class="item-rule">Nu-i insultați pe alți participanți</li>
-		<li class="item-rule">Nu exprimați parerea voastră dacă ea poate strica alții</li>
+		<li class="item-rule">Nu exprimați parerea voastră dacă ea poate dăuna altora</li>
 		<li class="item-rule">Daca scrieți o poezie,pastrați un ritm corect. Trebuie ca cititori să-i
-		citiească cu placere</li>
+		citească cu placere</li>
 		<li class="item-rule">Analfabetismul e înterzis. Dacă nu sunteți sigur, nu ne vizitați!</li>
-		<li class="item-rule">Respectati-l pe Abdul,Dumnezeu vostru, ca parintii voștri și egoismul vostru</li>
+		<li class="item-rule">Respectați-l pe Abdul,Dumnezeul vostru, ca parinții voștri și egoismul vostru</li>
 		<li class="item-rule">Fără politică</li>
-		<li class="item-rule">Admin e drăguț, îl iubiți și respectați. Așa e scris în Biboran</li>
-		<li class="item-rule">Îi mulțumim pe participanți cu propozițiile raționale</li>
-		<li class="item-rule">Nu întrerupeți legii teoriei de relativitate, de termodinamică si toatei 
-		fizicei, altfel v-o fac nevalabili la femei!!!</li>
+		<li class="item-rule">Adminul e drăguț, îl iubiți și respectați. Așa e scris în Biboran</li>
+		<li class="item-rule">Îi mulțumim pe participanți cu propoziții raționale</li>
+		<li class="item-rule">Nu întrerupeți legile teoriei de relativitate, de termodinamică si
+		fizice, altfel v-o fac nevalabili la femei!!!</li>
 		<li class="item-rule">Bine ați venit! Vă rugam,puneți întrebarile voastre administratorelor</li>
 	</ul>
 </div>
