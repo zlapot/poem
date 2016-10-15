3ad1d7deea5731b4d00bc3e21d084012715b22b9
dadofganja <pidorasi.sukableat@yandex.ru>
2016-10-15 17:33:40 +0500

    rules changement

--- views/moderator/rules/rules8.php
+++ views/moderator/rules/rules8.php
@@ -1,19 +1,40 @@
 <div class="col-md-4 offset-right blockquote-reverse">
-	Regulile acestui site pentru tovarăși romănești. Vă rog,mă scuzați,dacă veți
-	vedea greșeli
+Les règles du site pour la monde francophonique:
+
 	<ul class="poem-rule">
-		<li class="item-rule">Nu utilisați obsceni cuvânte</li>
-		<li class="item-rule">Nu-i insultați pe alți participanți</li>
-		<li class="item-rule">Nu exprimați parerea voastră dacă ea poate strica alții</li>
-		<li class="item-rule">Daca scrieți o poezie,pastrați un ritm corect. Trebuie ca cititori să-i
-		citiească cu placere</li>
-		<li class="item-rule">Analfabetismul e înterzis. Dacă nu sunteți sigur, nu ne vizitați!</li>
-		<li class="item-rule">Respectati-l pe Abdul,Dumnezeu vostru, ca parintii voștri și egoismul vostru</li>
-		<li class="item-rule">Fără politică</li>
-		<li class="item-rule">Admin e drăguț, îl iubiți și respectați. Așa e scris în Biboran</li>
-		<li class="item-rule">Îi mulțumim pe participanți cu propozițiile raționale</li>
-		<li class="item-rule">Nu întrerupeți legii teoriei de relativitate, de termodinamică si toatei 
-		fizicei, altfel v-o fac nevalabili la femei!!!</li>
-		<li class="item-rule">Bine ați venit! Vă rugam,puneți întrebarile voastre administratorelor</li>
+		<li class="item-rule">
+		Ne jurez et ne blasphémez pas
+		</li>
+		<li class="item-rule">
+		N'insultez pas les autres participants
+		</li>
+		<li class="item-rule">
+		N'exprimez pas votre avis,s'il peux undommager les autres
+		</li>
+		<li class="item-rule">
+		Lorsque vous écrivez une poème, gardez votre mètre correct
+		</li>
+		<li class="item-rule">
+		Vous ne pouvez pas être analphabètes, sinon quittez le site!!
+		</li>
+		<li class="item-rule">
+		Respecte Abdul,votre Dieu, comme vos parents, comme votre egoïsme et vos poux
+		</li>
+		<li class="item-rule">
+		Aucune politique!
+		</li>
+		<li class="item-rule">L'administrateur est mignon et vous devez l'aimer et le respecter
+		C'est absolut normal. Soyez aussi les mignons
+		</li>
+		<li class="item-rule">
+		Nous encourageons les participants avec des propositions utiles et précieux
+		</li>
+		<li class="item-rule">N'interrompez pas dans les lois de thérmodynamique, de relativité et,en fait,
+		n'interrompez pas dans les lois de physique,parbleu!!
+		</li>
+		<li class="item-rule">
+		Bienvenue à notre site.  Sentez-vous comme chez vous et demandez votre 
+		question à l'administration
+		</li>
 	</ul>
 </div>
