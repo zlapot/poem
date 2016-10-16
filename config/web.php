95bb6caf4ed88319e3265a873eab2cb8d5c2742a
zlapot <zloi.apot@yandex.ru>
2016-10-17 02:23:36 +0300

    Revert "create admin panel"
    This reverts commit 8d38172f7157898d162826c1c01ed6fdc836984c.

--- config/web.php
+++ config/web.php
@@ -6,11 +6,6 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
-    'modules'=>[
-        'admin' => [
-            'class' => 'app\modules\admin\Admin',
-        ],
-    ],
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
@@ -59,11 +54,10 @@ $config = [
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
-            'rules' => [                              
-                [
-                    'pattern' => '<controller>/<action>',
-                    'route' => '<controller>/<action>',
-                ],                
+            'rules' => [
+                '' => 'site/index',
+                'login' => 'site/login',
+                'logout' => 'site/logout',
             ],
 
         ],
