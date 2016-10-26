3ff6095e3202a744a2f404d20f637d875d827bbb
dadofganja <pidorasi.sukableat@yandex.ru>
2016-10-26 21:01:41 +0500

    fr corrextion

--- messages/fr/title.php
+++ messages/fr/title.php
@@ -7,6 +7,6 @@ return [
     'Наличие нецензурной лексики'=>'La presence des mots obscènes',
     'Название'=>'Le nom',
     'Опубликовать'=>'Publier',
-    'Ваша заявка на публикацию была принята, администрация рассмотрит ее в ближайшее время, спасибо за ваш вклад в развитие данного проекта. Просьба придерживаться правил (они есть на нашем сайте).'=>'Votre demande pour la publication était reçue, l\'administation va elle considerer dans le temps prochain, merci pour votre apport en dévéloppement de ce projet. Suivez les règles (elles sont dans notre site',
+    'Ваша заявка на публикацию была принята, администрация рассмотрит ее в ближайшее время, спасибо за ваш вклад в развитие данного проекта. Просьба придерживаться правил (они есть на нашем сайте).'=>'Votre demande pour la publication était reçue, l\'administation va la prendre en consideration dans le temps prochain, merci pour votre apport en dévéloppement de ce projet. Suivez les règles (elles sont dans notre site',
 
 ];
